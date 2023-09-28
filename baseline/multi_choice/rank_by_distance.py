840e09ec3fe00a021eadb7622e5ada967db6cea3
Darie Petcu <dariepetcu@gmail.com>
2023-09-28 23:05:50 +0200

    finished rank_by_distance script

--- baseline/multi_choice/rank_by_distance.py
+++ baseline/multi_choice/rank_by_distance.py
@@ -1,23 +1,39 @@
-import os
-from baseline.multi_choice.utils_multiple_choice import MuTualProcessor
+import json, tqdm
+import numpy as np
+from utils_multiple_choice import MuTualProcessor
 
 
-mutual_embeddings_path = ""
-mmlu_embeddings_path = ""
-split = ""
+mutual_embeddings_path = "baseline/embeddings/train.json"
+mmlu_embeddings_path = "baseline/embeddings/auxiliary_train.json"
+save_rankings_path = 'baseline/embeddings/rankings.json'
 
-mtl_p = MuTualProcessor()
-mtl_data = mtl_p._read_txt(os.path.join(mutual_embeddings_path, split))
+# compute mean cosine similarity between one row and all rows in embeddings
+def cosine_similarity(row, embeddings):
+    row = np.array(row, dtype=np.float64)
+    sim_scores = []
+    for data_row in embeddings.values():
+        data_row = np.array(data_row, dtype=np.float64)
+        sim_score = np.dot(row, data_row) / (np.linalg.norm(row) * np.linalg.norm(data_row))
+        sim_scores.append(sim_score)
 
-mmlu_p = MuTualProcessor()
-mmlu_data = mmlu_p._read_txt(os.path.join(mmlu_embeddings_path, split))
+    return np.mean(sim_scores)
 
-# sim_scores = {}
 
-for i, row in enumerate(mmlu_data):
-    pass
+with open(mutual_embeddings_path, 'r') as f:
+    mtl_embeddings = json.load(f)
+
+with open(mmlu_embeddings_path, 'r') as f:
+    mmlu_embeddings = json.load(f)
+
+mmlu_scores = {}
+
+for row, embedding in mmlu_embeddings:
     # compare row with mtl_data
-    # sim_score = cosine_similarity(row, mtl_data)
-    # sim_scores[i] = sim_score
+    mmlu_score = cosine_similarity(embedding, mtl_embeddings)
+    mmlu_scores[row] = mmlu_score
+    
 
-# sim_scores = sorted(sim_scores.items(), key=lambda x: x[1], reverse=True)
+mmlu_scores = sorted(mmlu_scores.items(), key=lambda x: x[1], reverse=True)
+
+with open(save_rankings_path, 'w') as f:
+    json.dump(mmlu_scores, f)
