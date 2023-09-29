698dac7dc9f7c891c17d4a67f0fb396eac07060e
Alexandru Turcu <scur0669@int6.local.snellius.surf.nl>
2023-09-29 14:15:55 +0200

    small changes in sh

--- jobs/train_classic.sh
+++ jobs/train_classic.sh
@@ -11,5 +11,5 @@
 # Execute program located in $HOME
 source activate dl4nlp
 
-cd RibFrac
+cd MuTual
 srun python -u baseline/multi_choice/run_multiple_choice.py --percentage 0.5 --data_dir "data/mutual_plus" --model_type "roberta" --model_name_or_path "roberta-base" --task_name "mutual" --output_dir "output" --do_train
