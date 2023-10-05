1765bf03c9f961f656adfd1077132e346bd3bbb7
xqz-u <marco.gallo0530@gmail.com>
2023-10-05 03:05:19 +0200

    feat: fine tuning notebook.
    TODO check that nothing weird is happening with caching (restarting the
    kernel gives the same np.random.choice in the last cell every time?!)

---
+++ similarity_augmentations/conf.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+# invoke from root of package: python -m baseline.conf
+TOKENIZED_DATASET_DIR = Path("tokenized_datasets")
+
+VECTORDB_DIR = Path("vector_db")
+
+FINETUNED_MODELS_DIR = Path("finetuned_models")
